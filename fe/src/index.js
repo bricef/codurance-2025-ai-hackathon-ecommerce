b608aa4c8b57a47d8481c51d663bb3002a5aeebc
Brice Fernandes <brice.fernandes@raspberrypi.org>
2025-04-26 12:00:26 +0100

    Get supabase client working from fe build

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,2 +1,22 @@
-import {supabase} from "./supabase"
+import {client} from "./supabase"
+
+
+async function fetchTestTable() {
+    try {
+        const { data, error } = await client
+            .from('test_table')
+            .select(
+                `
+                id,
+                created_at
+                `,
+        );
+        console.log(data)
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      console.error('Error fetching test table:', error);
+    }
+  }
 
--- fe/src/supabase.js
+++ fe/src/supabase.js
@@ -1,22 +1,10 @@
-import {supabaseClient} from "../../be/src/supabase-client"
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://mguedjuqereibecvopcn.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ndWVkanVxZXJlaWJlY3ZvcGNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjE2OTcsImV4cCI6MjA2MTIzNzY5N30.oHfANJe9LpYoN9gtsZrOX8mXB4cnKILnFdIu3tuLYy4'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export const client = supabase;
 
 
-async function fetchTestTable() {
-    try {
-        const { data, error } = await supabaseClient
-            .from('test_table')
-            .select(
-                `
-                id,
-                created_at
-                `,
-        );
-        console.log(data)
-      if (error) {
-        throw error;
-      }
-    } catch (error) {
-      console.error('Error fetching test table:', error);
-    }
-  }
 
