dc695ef51eee23594e65e274250e714185d0182c
Brice Fernandes <brice.fernandes@raspberrypi.org>
2025-04-26 12:27:34 +0100

    link up to sb

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,22 +1,23 @@
+
+import Alpine from 'alpinejs'
 import {client} from "./supabase"
 
 
 async function fetchTestTable() {
-    try {
-        const { data, error } = await client
-            .from('test_table')
-            .select(
-                `
-                id,
-                created_at
-                `,
-        );
-        console.log(data)
-      if (error) {
-        throw error;
-      }
-    } catch (error) {
+    const { data, error } = await client
+        .from('styles_slim')
+        .select(`id,gender,masterCategory,subCategory,articleType,baseColour,season,year,usage,productDisplayName`);
+      
+    if (error) {
       console.error('Error fetching test table:', error);
+      return [];
     }
+    return data;
   }
+
+const products = fetchTestTable()
+window.products = products
+
+
+Alpine.start()
 
